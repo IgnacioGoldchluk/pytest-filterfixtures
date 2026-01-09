cc1c9b99e97b9e0f04b8241f8f260baee31ad040
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2026-01-09 13:41:17 +0200

    initial working examples

--- src/pytest_filterfixtures/plugin.py
+++ src/pytest_filterfixtures/plugin.py
@@ -10,8 +10,6 @@ def pytest_addoption(parser: pytest.Parser):
         "--exclude-fixtures",
         dest="exclude_fixtures",
         nargs="+",
-        default=set(),
-        type=set,
         required=False,
         help="Ignores tests that use any of the fixtures provided",
     )
@@ -19,8 +17,6 @@ def pytest_addoption(parser: pytest.Parser):
         "--include-fixtures",
         dest="include_fixtures",
         nargs="+",
-        default=set(),
-        type=set,
         required=False,
         help="Collects only the tests that use at least one of the fixtures provided",
     )
@@ -30,19 +26,16 @@ def pytest_addoption(parser: pytest.Parser):
 def pytest_collection_modifyitems(
     session: pytest.Session, config: pytest.Config, items: list[pytest.Item]
 ):
-    include: set[str] = set(config.getoption("include_fixtures"))
-    exclude: set[str] = set(config.getoption("exclude_fixtures"))
-
     tmp_items = items
 
-    if exclude:
+    if exclude := config.getoption("exclude_fixtures"):
         tmp_items = [
             item
             for item in tmp_items
             if not any(f in exclude for f in getattr(item, "fixturenames", []))
         ]
 
-    if include:
+    if include := config.getoption("include_fixtures"):
         tmp_items = [
             item
             for item in tmp_items
--- tests/test_filterfixtures.py
+++ tests/test_filterfixtures.py
@@ -5,10 +5,26 @@ pytest_plugins = ["pytester"]
 EXAMPLE_PATH = "examples/test_examples.py"
 
 
+@pytest.fixture(autouse=True)
+def copy_example_test(pytester: pytest.Pytester):
+    pytester.copy_example(EXAMPLE_PATH)
+
+
+def test_no_options_runs_everything(pytester: pytest.Pytester):
+    result = pytester.runpytest("-v")
+    result.stdout.fnmatch_lines(
+        [
+            "*test_examples.py::test_example1 PASSED*",
+            "*test_examples.py::test_example2 PASSED*",
+        ]
+    )
+    assert result.ret == 0
+    result.assert_outcomes(passed=2)
+
+
 class TestInclude:
-    def test_no_options_runs_everything(self, pytester: pytest.Pytester):
-        pytester.copy_example(EXAMPLE_PATH)
-        result = pytester.runpytest("-v")
+    def test_includes_autoused_fixture(self, pytester: pytest.Pytester):
+        result = pytester.runpytest("--include-fixtures", "example_autoused", "-v")
         result.stdout.fnmatch_lines(
             [
                 "*test_examples.py::test_example1 PASSED*",
@@ -18,8 +34,21 @@ class TestInclude:
         assert result.ret == 0
         result.assert_outcomes(passed=2)
 
-    def test_excludes_autoused_fixture(self, pytester: pytest.Pytester):
-        pytester.copy_example(EXAMPLE_PATH)
-        result = pytester.runpytest("-v", "--include-fixtures", "autoused")
+    def test_includes_multiple_fixtures(self, pytester: pytest.Pytester):
+        result = pytester.runpytest(
+            "--include-fixtures", "example_fixture_1", "example_fixture_2", "-v"
+        )
+        result.stdout.fnmatch_lines(
+            [
+                "*test_examples.py::test_example1 PASSED*",
+                "*test_examples.py::test_example2 PASSED*",
+            ]
+        )
         assert result.ret == 0
-        result.assert_outcomes(deselected=2)
+        result.assert_outcomes(passed=2)
+
+    def test_includes_only_specified_fixture(self, pytester: pytest.Pytester):
+        result = pytester.runpytest("--include-fixtures", "example_fixture_1", "-v")
+        result.stdout.fnmatch_lines("*test_examples.py::test_example1 PASSED*")
+        assert result.ret == 0
+        result.assert_outcomes(passed=1)
